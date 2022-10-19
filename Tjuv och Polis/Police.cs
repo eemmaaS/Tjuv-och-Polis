a076aa4a76424c5b6962e898f137541209dc3981
KaveCoding <113701512+KaveCoding@users.noreply.github.com>
2022-10-19 14:20:05 +0200

    Fixed interactions

--- Tjuv och Polis/Police.cs
+++ Tjuv och Polis/Police.cs
@@ -18,13 +18,37 @@ namespace Tjuv_och_Polis
         {
             if (person is Thief)
             {
-                if(person.Inventory != null)
+                if(person.Inventory.Count >= 1)
                 {
-                    this.Inventory.AddRange(person.Inventory);
-                    person.Inventory.Clear();
+                    this.Inventory.AddRange(person.Inventory); //tar tjuvens saker och lägger dem i sin egen inventory
+                    person.Inventory.Clear(); //tömmer tjuvens inventory
+                    Console.WriteLine("Personindex (Thief) " + indexi + " had their loot taken from person Personindex (Police) " + indexj);
+                    Thread.Sleep(2000);
                 }
+                else
+                {
+                    Console.WriteLine("Personindex (Thief) " + indexi + " was inspected by personindex (Police) " + indexj);
+                    Thread.Sleep(2000);
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine("Thief inventory: ");
+
+                foreach (Thing thing in person.Inventory)
+                {
+                    Console.WriteLine(thing);
+                }
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Police inventory: ");
+
+                foreach (Thing thing in this.Inventory)
+                {
+                    Console.WriteLine(thing);
+                }
+                Console.ReadKey();
             }
             return person;
         }
+        
     }
 }
--- Tjuv och Polis/Thief.cs
+++ Tjuv och Polis/Thief.cs
@@ -18,18 +18,39 @@ namespace Tjuv_och_Polis
         {
             if(person is Citizen)
             {
-                if (person.Inventory != null && person.Inventory.Count > 0)
+                if (person.Inventory.Count > 0)
                 {
                     Random random = new Random();
                     int rnd = random.Next(0, person.Inventory.Count - 1);
                     string stolen_object = person.Inventory[rnd].ToString();
                     this.Inventory.Add(person.Inventory[rnd]);
                     person.Inventory.RemoveAt(rnd);
-                    Console.Write("Thief " + indexi + " has stolen " + stolen_object + " from  citizen " + indexj);
-                    Console.ReadKey();
+                    Console.Write("Personindex (Thief) " + indexi + " has stolen " + stolen_object + " from personindex (Citizen) " + indexj);
+                    Thread.Sleep(2000);
                 }
                
-                   
+                else
+                {
+                    Console.Write("Personindex (Thief) " + indexi + " punches personindex (Citizen) " + indexj);
+                    Thread.Sleep(2000);
+                }
+                Console.WriteLine(" ");
+
+                Console.WriteLine("Citizen inventory: ");
+                foreach (Thing thing in person.Inventory)
+                {
+                    Console.WriteLine(thing);
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine("Thief inventory: ");
+                
+                foreach (Thing thing in this.Inventory)
+                {
+                    Console.WriteLine(thing);
+                }
+
+                Console.ReadKey();
+
 
 
             }
